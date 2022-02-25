8f72eed0a3a6ab88109b8a16f77707726ec4fd14
Michael Chung <mikechung89@gmail.com>
2022-02-25 10:59:52 -0500

    commit tree c637b683ce1236fec2b05343764049c537251d18

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // code your solution here
+// function winFind(win) {
+//     return win.result === "W"
+// }
+
+// function superbowlWin(array) {
+//     const req = array.find(winFind);
+//     if (req) {
+//         return req.year
+//     } else return undefined
+// }
+
+
+// function superbowlWin(array) {
+//   const found = array.find(w => w.result === "W");
+//     if (found) {
+//         return found.year
+//     }
+// }
+
+// use ternary
+// function superbowlWin(array) {
+//     const found = array.find(w => w.result === "W");
+//     return found ? found.year: undefined;
+// }
+
+//refactored
+
+function superbowlWin(array) {
+    const found = array.find(w => w.result === "W");
+    return found && found.year
+}
